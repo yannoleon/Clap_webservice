c504fe142c63b57ab07ed6354e33136a4c2f1804
yannoleon <login@yanncherdo.fr>
2024-10-22 20:14:16 +0200

    more

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Response
+from transformers import AutoTokenizer, ClapTextModelWithProjection
+from sklearn.metrics.pairwise import cosine_similarity
+from pydantic import BaseModel
+import uvicorn
+import os
+
+# ----------------------------MODEL-----------------------------------------------------
+
+model = ClapTextModelWithProjection.from_pretrained("laion/clap-htsat-unfused")
+tokenizer = AutoTokenizer.from_pretrained("laion/clap-htsat-unfused")
+
+# -----------------------------APP------------------------------------------------------
+
+app = FastAPI()
+
+class Text_entry(BaseModel):
+    text: list[str]
+
+@app.get('/')
+def root():
+    return Response("<h1> Texts to cosines app </h1>")
+
+@app.post('/text_cosine', response_model=Text_entry)
+def text_cosine(text_entry:Text_entry):
+
+    inputs = tokenizer(text_entry.text, padding=True, return_tensors="pt")
+
+    outputs = model(**inputs)
+    text_embeds = outputs.text_embeds.detach().numpy()
+
+    s = str(cosine_similarity(text_embeds))
+
+    texts_str = ''.join([t + '\n' for t in text_entry.text])
+
+    res = f"<h1> Cosine similarities between:\n {texts_str} is:\n {s} </h1>"
+
+    return Response(res)
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    dev_host = "127.0.0.1"
+    uvicorn.run(app, host=host, port=int(os.environ.get("PORT", 8080)))
